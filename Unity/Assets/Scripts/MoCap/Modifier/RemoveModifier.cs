5d737e10b9e32f718a2903b44581fd05371bccae
Stefan Marks <stefan.marks.ac@gmail.com>
2016-02-19 08:29:35 +1300

    Added RemoveModifier

---
+++ Unity/Assets/Scripts/MoCap/Modifier/RemoveModifier.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using MoCap;
+
+/// <summary>
+/// Component for scaline MoCap data.
+/// </summary>
+///
+[AddComponentMenu("Motion Capture/Modifier/Remove")]
+[DisallowMultipleComponent]
+public class RemoveModifier : MonoBehaviour, IModifier
+{
+	[Tooltip("Prefix for any bone or marker name.")]
+	public string namePrefix = "";
+
+	[Tooltip("Names of bones or markers to always disable.")]
+	public string[] names = { };
+
+
+	public void Start()
+	{
+		// empty, but necessary to get the "Enable" button in the inspector
+	}
+
+
+	public void Process(ref MoCapData data)
+	{
+		if (!enabled) return;
+
+		foreach (string name in names)
+		{
+			if (data.buffer.Name.Equals(namePrefix + name))
+			{
+				data.tracked = false;
+				break;
+			}
+		}
+	}
+}
