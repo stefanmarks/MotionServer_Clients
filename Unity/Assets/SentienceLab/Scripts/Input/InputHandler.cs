0260528077d6c9ea2cd822ecd995f2ec95d69e11
Stefan Marks <stefan.marks.ac@gmail.com>
2016-10-04 06:49:16 +1300

    Changed Action handlers to Input handlers

---
+++ Unity/Assets/SentienceLab/Scripts/Input/InputHandler.cs
@@ -0,0 +1,491 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SentienceLab.Input
+{
+	/// <summary>
+	/// Class for managing inputs via a variety of sources, e.g., keypress, mouse click, etc.
+	/// </summary>
+	///
+	public class InputHandler
+	{
+		public readonly string Name;
+
+
+		/// <summary>
+		/// Shortcut to find an input handler through the InputManager singelton.
+		/// </summary>
+		/// <param name="inputName">the input handler to search for</param>
+		/// <returns>the input handler</returns>
+		///
+		public static InputHandler Find(String inputName)
+		{
+			return InputManager.GetInputHandler(inputName);
+		}
+
+
+		/// <summary>
+		/// Creates a new input handler instance.
+		/// </summary>
+		/// 
+		public InputHandler(string name)
+		{
+			Name = name;
+			devices = new List<IDevice>();
+
+			// by default, check for existing Unity Input axis names
+			try
+			{
+				UnityEngine.Input.GetAxis(name);
+				AddMapping(InputManager.InputType.UnityInput, name);
+			}
+			catch (Exception)
+			{
+				// axis doesn't exist > don't bother
+			}
+
+			// set default threshold
+			SetPressThreshold(1.0f);
+		}
+
+
+		/// <summary>
+		/// Adds an input handler.
+		/// </summary>
+		/// <param name="type">the type of input handler</param>
+		/// <param name="inputName">the name of the input handler</param>
+		/// <returns><c>true</c>if the handler was added</returns>
+		/// 
+		public bool AddMapping(InputManager.InputType type, string inputName)
+		{
+			IDevice device = null;
+			// create specific device subtype
+			switch (type)
+			{
+				case InputManager.InputType.UnityInput:  device = new Device_UnityInput(); break;
+				case InputManager.InputType.MoCapDevice: device = new Device_MoCap(); break;
+				case InputManager.InputType.Keyboard:    device = new Device_Keyboard(); break;
+			}
+			// try to initialise given the parameter string
+			if ((device != null) && device.Initialise(inputName))
+			{
+				// success
+				devices.Add(device);
+			}
+			else
+			{
+				// failed
+				device = null;
+			}
+			return (device != null);
+		}
+
+
+		/// <summary>
+		/// Processes all input devices.
+		/// </summary>
+		/// 
+		public void Process()
+		{
+			foreach (IDevice d in devices)
+			{
+				d.Process();
+			}
+		}
+
+
+		/// <summary>
+		/// Checks if the input is currently active, e.g., a button is down.
+		/// </summary>
+		/// <returns><c>true</c> when the input is active</returns>
+		///
+		public bool IsActive()
+		{
+			bool returnValue = false;
+			foreach (IDevice d in devices)
+			{
+				returnValue |= (d.GetValue() > 0);
+			}
+			return returnValue;
+		}
+
+
+		/// <summary>
+		/// Checks if the input has just been activated, e.g., a button has been pressed.
+		/// </summary>
+		/// <returns><c>true</c> when the input has been activated</returns>
+		///
+		public bool IsActivated()
+		{
+			bool isActivated = false;
+			foreach (IDevice d in devices)
+			{
+				isActivated |= d.IsActivated();
+			}
+			return isActivated;
+		}
+
+
+		/// <summary>
+		/// Checks if the input has been deactivated, e.g., a button has been released.
+		/// </summary>
+		/// <returns><c>true</c> when the input has been deactivated</returns>
+		///
+		public bool IsDeactivated()
+		{
+			bool isDeactivated = false;
+			foreach (IDevice d in devices)
+			{
+				isDeactivated |= d.IsDeactivated();
+			}
+			return isDeactivated;
+		}
+
+
+		/// <summary>
+		/// Gets the raw "value" of the input.
+		/// </summary>
+		/// <returns>raw channel/axis/button value</returns>
+		///
+		public float GetValue()
+		{
+			float returnValue = 0.0f;
+			foreach (IDevice d in devices)
+			{
+				returnValue += d.GetValue();
+			}
+			return returnValue;
+		}
+
+
+		public float GetPressThreshold()
+		{
+			return pressThreshold;
+		}
+
+
+		public void SetPressThreshold(float value)
+		{
+			pressThreshold = value;
+			foreach (IDevice device in devices)
+			{
+				device.SetPressThreshold(value);
+			}
+		}
+
+
+		public override string ToString()
+		{
+			string txtDevices = "";
+			foreach (IDevice d in devices)
+			{
+				txtDevices += (txtDevices.Length > 0) ? ", " : "";
+				txtDevices += d.ToString();
+			}
+			return "'" + Name + "' (Devices: " + txtDevices + ")";
+		}
+
+
+		private List<IDevice> devices;
+		private float         pressThreshold;
+
+
+		/// <summary>
+		/// Interface for a generic input device.
+		/// </summary>
+		///
+		private interface IDevice
+		{
+			bool  Initialise(string inputName);
+			void  Process();
+			bool  IsActivated();
+			bool  IsDeactivated();
+			float GetValue();
+			void  SetPressThreshold(float value);
+		}
+
+		/// <summary>
+		/// Class for a Unity input axis device.
+		/// </summary>
+		/// 
+		private class Device_UnityInput : IDevice
+		{
+			public Device_UnityInput()
+			{
+				axisName = "";
+				oldValue = 0;
+			}
+
+			public bool Initialise(string inputName)
+			{
+				bool success = false;
+				try
+				{
+					UnityEngine.Input.GetAxis(inputName);
+					success = true;
+					axisName = inputName;
+					// get first value
+					Process();
+					oldValue = value;
+				}
+				catch (Exception e)
+				{
+					// the Input.GetAxis call didn't succeed
+					// -> the name of the key must have been wrong
+					Debug.LogWarning(e.Message);
+				}
+				return success;
+			}
+
+			public void Process()
+			{
+				oldValue = value;
+				value    = UnityEngine.Input.GetAxis(axisName);
+			}
+
+			public bool IsActivated()
+			{
+				return (oldValue < pressThreshold) && (value >= pressThreshold);
+			}
+
+			public bool IsDeactivated()
+			{
+				return (oldValue >= pressThreshold) && (value < pressThreshold);
+			}
+
+			public float GetValue()
+			{
+				return value;
+			}
+
+			public void SetPressThreshold(float value)
+			{
+				pressThreshold = value;
+			}
+
+			public override string ToString()
+			{
+				return "Axis '" + axisName + "'";
+			}
+
+			private string axisName;
+			private float  value, oldValue;
+			private float  pressThreshold;
+		}
+
+
+
+		/// <summary>
+		/// Class for a MoCap input device.
+		/// </summary>
+		///
+		private class Device_MoCap : IDevice
+		{
+			public Device_MoCap()
+			{
+				device = null;
+			}
+
+			public bool Initialise(string inputName)
+			{
+				string[] parts = inputName.Split('/');
+				if (parts.Length >= 2)
+				{
+					device = new MoCap.InputDeviceHandler(parts[0], parts[1]);
+				}
+				return true;
+			}
+
+			public void Process()
+			{
+				// nothing to do
+			}
+
+			public bool IsActivated()
+			{
+				return device.GetButtonDown();
+			}
+
+			public bool IsDeactivated()
+			{
+				return device.GetButtonUp();
+			}
+
+			public float GetValue()
+			{
+				return device.GetAxis();
+			}
+
+			public void SetPressThreshold(float value)
+			{
+				device.PressThreshold = value;
+			}
+
+			public override string ToString()
+			{
+				return device.ToString();
+			}
+
+			private MoCap.InputDeviceHandler device;
+		}
+
+
+		/// <summary>
+		/// Class for a keyboard button input device.
+		/// It can be a single key, or a +/- combination of two keys, e.g., for zoom.
+		/// </summary>
+		/// 
+		private class Device_Keyboard : IDevice
+		{
+			enum Mode
+			{
+				SingleKey,
+				PlusMinus,
+				Combination
+			}
+
+			public Device_Keyboard()
+			{
+				keyName1 = "";
+				keyName2 = "";
+			}
+
+			public bool Initialise(string inputName)
+			{
+				bool success = false;
+				try
+				{
+					if ((inputName.Length > 1) && inputName.Contains("/"))
+					{
+						// input name for a plus/minus combination (with a '/' separating them)
+						string[] parts = inputName.Split('/');
+
+						UnityEngine.Input.GetKey(parts[0]);
+						keyName1 = parts[0];
+						UnityEngine.Input.GetKey(parts[1]);
+						keyName2 = parts[1];
+
+						mode = Mode.PlusMinus;
+					}
+					else if ((inputName.Length > 1) && inputName.Contains("+"))
+					{
+						// input name for a combo name (with a '+' separating them)
+						string[] parts = inputName.Split('+');
+
+						UnityEngine.Input.GetKey(parts[0]);
+						keyName1 = parts[0];
+						UnityEngine.Input.GetKey(parts[1]);
+						keyName2 = parts[1];
+
+						mode = Mode.Combination;
+					}
+					else
+					{
+						// only a single key
+						UnityEngine.Input.GetKey(inputName);
+						keyName1 = inputName;
+						mode = Mode.SingleKey;
+					}
+
+					success = true;
+				}
+				catch (Exception e)
+				{
+					// one of the Input.GetKey calls didn't succeed
+					// -> the name of the key must have been wrong
+					Debug.LogWarning(e.Message);
+				}
+				return success;
+			}
+
+			public void Process()
+			{
+				// nothing to do
+			}
+
+			public bool IsActivated()
+			{
+				switch (mode)
+				{
+					case Mode.PlusMinus:
+						return UnityEngine.Input.GetKeyDown(keyName1) ||
+							   UnityEngine.Input.GetKeyDown(keyName2);
+
+					case Mode.Combination:
+						return (UnityEngine.Input.GetKeyDown(keyName1) &&
+								UnityEngine.Input.GetKey(keyName2)) ||
+							   (UnityEngine.Input.GetKey(keyName1) &&
+								UnityEngine.Input.GetKeyDown(keyName2));
+
+					default:
+						return UnityEngine.Input.GetKeyDown(keyName1);
+				}
+			}
+
+			public bool IsDeactivated()
+			{
+				switch (mode)
+				{
+					case Mode.PlusMinus:
+						return UnityEngine.Input.GetKeyUp(keyName1) ||
+							   UnityEngine.Input.GetKeyUp(keyName2);
+
+					case Mode.Combination:
+						return (UnityEngine.Input.GetKeyUp(keyName1) &&
+								UnityEngine.Input.GetKey(keyName2)) ||
+							   (UnityEngine.Input.GetKey(keyName1) &&
+								UnityEngine.Input.GetKeyUp(keyName2));
+
+					default:
+						return UnityEngine.Input.GetKeyUp(keyName1);
+				}
+			}
+
+			public float GetValue()
+			{
+				float value;
+				switch (mode)
+				{
+					case Mode.PlusMinus:
+						value =   (UnityEngine.Input.GetKey(keyName1) ? -1 : 0)
+								+ (UnityEngine.Input.GetKey(keyName2) ? 1 : 0);
+						break;
+
+					case Mode.Combination:
+						value = (UnityEngine.Input.GetKey(keyName1) &&
+								 UnityEngine.Input.GetKey(keyName2)) ? 1 : 0;
+						break;
+
+					default:
+						value = UnityEngine.Input.GetKey(keyName1) ? 1 : 0;
+						break;
+				}
+				return value;
+			}
+
+			public void SetPressThreshold(float value)
+			{
+				// doesn't apply to keys
+			}
+
+			public override string ToString()
+			{
+				switch (mode)
+				{
+					case Mode.PlusMinus:
+						return "Keys '" + keyName1 + "'/'" + keyName2 + "'";
+
+					case Mode.Combination:
+						return "Keys '" + keyName1 + "'+'" + keyName2 + "'";
+
+					default:
+						return "Key '" + keyName1 + "'";
+				}
+			}
+
+			private string keyName1, keyName2;
+			private Mode mode;
+		}
+	}
+}
--- Unity/Assets/SentienceLab/Scripts/Input/InputManager.cs
+++ Unity/Assets/SentienceLab/Scripts/Input/InputManager.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEditorInternal;
 #endif
 
-namespace VR.Input
+namespace SentienceLab.Input
 {
 	/// <summary>
-	/// Class for managing the mapping of input devices to actions.
-	/// This class is a simplication and extension at the same time of the Unity Input manager.
+	/// Class for managing the mapping of input devices to input handlers.
+	/// This class is a simplification and extension at the same time of the Unity Input manager.
 	/// 
-	/// Make sure the InputManager script is executed AFTER the MoCapManager, but before any other script in "Project Settings/Script Execution Order".
+	/// Make sure the InputManager script is executed AFTER the MoCapManager, 
+	/// but before any other script in "Project Settings/Script Execution Order".
 	/// </summary>
 	///
 
@@ -27,85 +29,129 @@ namespace VR.Input
 		///
 		public enum InputType
 		{
-			Keyboard,
-			Mouse,
-			Joystick,
-			Touch,
-			MoCapDevice
+			UnityInput,
+			MoCapDevice,
+			Keyboard
 		}
 
 
 		/// <summary>
-		/// Structure for associating an action name to a specific input device.
+		/// Structure for associating a handler name to a specific input device.
 		/// </summary>
 		///
 		[System.Serializable]
-		public struct ActionMap
+		public struct InputMap
 		{
-			public string    action;     // name of the action
-			public InputType inputType;  // type of the input
 			public string    inputName;  // name of the input
+			public InputType inputType;  // type of the input
+			public string    parameters; // parameters
 		}
 
-		[Tooltip("List of mappings of action names to keys/devices/etc.")]
-		public List<ActionMap> mappings;
+
+		[Tooltip("Resource with mappings of inputs to keys/devices/etc.")]
+		public TextAsset mappingFile;
+
+		[Tooltip("List of mappings of inputs to keys/devices/etc.")]
+		[HideInInspector]
+		public List<InputMap> mappings;
 
 
 		public void Start()
 		{
+			ReadMappings();
 			ParseMappings();
+
+			// output list of input handlers
+			string logTxt = "";
+			foreach (InputHandler h in handlers.Values)
+			{
+				logTxt += ((logTxt.Length > 0) ? ", " : "") + h.ToString();
+		}
+			Debug.Log("Loaded input handlers: " + logTxt);
+		}
+
+
+		private void ReadMappings()
+		{
+			if (mappingFile == null) return;
+
+			// JSON file cannot directly be read because of polymorphism > split manually
+			// remove tabs and linefeeds
+			string txtConfig = mappingFile.text.Replace("\t", "").Replace("\n", "");
+			// cut beginning and end
+			txtConfig = Regex.Replace(txtConfig, "^\\s*{\\s*\"Inputs\":\\[\\s*{", "");
+			txtConfig = Regex.Replace(txtConfig, "}\\s*\\]\\s*}\\s*$", "");
+			// split
+			string[] inputMappings = Regex.Split(txtConfig, "}\\s*,\\s*{");
+
+			foreach (string configTxt in inputMappings)
+			{
+				try
+		{
+					string configTxtTrim = "{" + configTxt + "}";
+					InputMap map = JsonUtility.FromJson<InputMap>(configTxtTrim);
+					InputHandler handler;
+					if (!handlers.TryGetValue(map.inputName, out handler))
+			{
+						handler = new InputHandler(map.inputName);
+						handlers[map.inputName] = handler;
+					}
+					handler.AddMapping(map.inputType, map.parameters);
+				}
+				catch (System.Exception e)
+				{
+					Debug.LogWarning("Could not parse Input Mapping Configuration entry " +
+						"'" + configTxt + "'" +
+						" - Reason: " + e.Message);
+				}
+			}
 		}
 
 
 		private void ParseMappings()
 		{
 			// create/update handlers
-			foreach (ActionMap map in mappings)
+			foreach (InputMap map in mappings)
 			{
-				ActionHandler handler;
-				if (!handlers.TryGetValue(map.action, out handler))
-				{
-					handler = new ActionHandler(map.action);
-					handlers[map.action] = handler;
+				InputHandler handler;
+				if (!handlers.TryGetValue(map.inputName, out handler))
+			{
+					handler = new InputHandler(map.inputName);
+					handlers[map.inputName] = handler;
 				}
-				handler.AddMapping(map.inputType, map.inputName);
+				handler.AddMapping(map.inputType, map.parameters);
 			}
-
-			// output list of handlers
-			string logTxt = "";
-			foreach (ActionHandler h in handlers.Values)
-			{
-				logTxt += ((logTxt.Length > 0) ? ", " : "") + h.ToString();
-			}
-			Debug.Log("Loaded action handlers: " + logTxt);
 		}
 
 
 		public void Update()
 		{
-			// nothing to do here
+			foreach (InputHandler handler in handlers.Values)
+			{
+				handler.Process();
+			}
 		}
 
 
 		/// <summary>
-		/// Retrieves a specific action handler based on the name.
+		/// Retrieves a specific input handler based on the name.
 		/// </summary>
 		/// <param name="name">the handler name to look for</param>
-		/// <returns>the action handler</returns>
+		/// <returns>the input handler</returns>
 		/// 
-		public static ActionHandler GetActionHandler(string name)
+		public static InputHandler GetInputHandler(string name)
 		{
-			ActionHandler handler;
+			InputHandler handler;
 			if (!handlers.TryGetValue(name, out handler))
 			{
-				handler = new ActionHandler(name);
+				handler = new InputHandler(name);
 				handlers[name] = handler;
 			}
 			return handler;
 		}
 
 
-		private static Dictionary<string, ActionHandler> handlers = new Dictionary<string, ActionHandler>();
+		private static Dictionary<string, InputHandler> handlers = new Dictionary<string, InputHandler>();
 	}
 
 }
@@ -118,7 +164,7 @@ namespace VR.Input
 /// </summary>
 /// 
 
-[CustomEditor(typeof(VR.Input.InputManager))]
+[CustomEditor(typeof(SentienceLab.Input.InputManager))]
 public class InputManagerEditor : Editor
 {
 	private ReorderableList list;
@@ -136,22 +182,23 @@ public class InputManagerEditor : Editor
 				float w = (rect.width - 100) / 2 - 5;
 				EditorGUI.PropertyField(
 					new Rect(rect.x, rect.y, w, EditorGUIUtility.singleLineHeight),
-					element.FindPropertyRelative("action"), GUIContent.none);
+					element.FindPropertyRelative("inputName"), GUIContent.none);
 				EditorGUI.PropertyField(
 					new Rect(rect.x + w + 5, rect.y, 100, EditorGUIUtility.singleLineHeight),
 					element.FindPropertyRelative("inputType"), GUIContent.none);
 				EditorGUI.PropertyField(
 					new Rect(rect.xMax - w, rect.y, w, EditorGUIUtility.singleLineHeight),
-					element.FindPropertyRelative("inputName"), GUIContent.none);
+					element.FindPropertyRelative("parameters"), GUIContent.none);
 			};
 		list.drawHeaderCallback = (Rect rect) => {
-			EditorGUI.LabelField(rect, "Actions");
+			EditorGUI.LabelField(rect, "Input List");
 		};
 	}
 
 	public override void OnInspectorGUI()
 	{
 		serializedObject.Update();
+		base.OnInspectorGUI();
 		list.DoLayoutList();
 		serializedObject.ApplyModifiedProperties();
 	}
