a17d9a5cf67363cad6020c8d97ffc80cad660b6c
Stefan Marks <stefan.marks.ac@gmail.com>
2017-01-19 13:27:13 +1300

    Rotation added to MovementController

--- Unity/Assets/SentienceLab/Scripts/Tools/MovementController.cs
+++ Unity/Assets/SentienceLab/Scripts/Tools/MovementController.cs
@@ -9,11 +9,13 @@ public class MovementController : MonoBehaviour
 	public string actionMoveX         = "moveX";
 	public string actionMoveY         = "moveY";
 	public string actionMoveZ         = "moveZ";
-	public float  maxTranslationSpeed =  1.0f;
+	public float  maxTranslationSpeed = 1.0f;
+	public float  translationLerp     = 1.0f;
 
 	public string actionRotateX       = "rotateX";
 	public string actionRotateY       = "rotateY";
-	public float  maxRotationSpeed    = 90.0f;
+	public float  maxRotationSpeed    = 45.0f;
+	public float  rotationLerp        = 1.0f;
 
 	public Transform rotationBasisNode;
 
@@ -25,7 +27,9 @@ public class MovementController : MonoBehaviour
 		handlerMoveZ   = (actionMoveZ.Length == 0)   ? null : InputHandler.Find(actionMoveZ);
 		handlerRotateX = (actionRotateX.Length == 0) ? null : InputHandler.Find(actionRotateX);
 		handlerRotateY = (actionRotateY.Length == 0) ? null : InputHandler.Find(actionRotateY);
-		vec = new Vector3();
+		vecTranslate   = new Vector3();
+		vecRotate      = new Vector3();
+		vec            = new Vector3();
 
 		if (rotationBasisNode == null)
 		{
@@ -36,44 +40,37 @@ public class MovementController : MonoBehaviour
 
 	void Update() 
 	{
-		if (handlerRotateX != null)
-		{
-			// rotate up/down (always absolute around X axis)
-			transform.RotateAround(rotationBasisNode.position, rotationBasisNode.right, handlerRotateX.GetValue() * maxRotationSpeed * Time.deltaTime);
-		}
+		Vector3 vecR = Vector3.zero;
+		vecR.x = (handlerRotateX != null) ? handlerRotateX.GetValue() : 0;
+		vecR.y = (handlerRotateY != null) ? handlerRotateY.GetValue() : 0;
+		vecRotate = Vector3.Lerp(vecRotate, vecR, rotationLerp);
 
-		if (handlerRotateY != null)
-		{
-			// rotate left/right (always absolute around Y axis)
-			transform.RotateAround(rotationBasisNode.position, Vector3.up, handlerRotateY.GetValue() * maxRotationSpeed * Time.deltaTime);
-		}
+		// rotate up/down (always absolute around X axis)
+		transform.RotateAround(rotationBasisNode.position, rotationBasisNode.right, vecRotate.x * maxRotationSpeed * Time.deltaTime);
+		// rotate left/right (always absolute around Y axis)
+		transform.RotateAround(rotationBasisNode.position, Vector3.up, vecRotate.y * maxRotationSpeed * Time.deltaTime);
 
-		if (handlerMoveZ != null)
-		{
-			// calculate level forward (Z) direction of camera
-			vec = rotationBasisNode.forward; vec.y = 0; vec.Normalize();
-			// translate forward
-			transform.Translate(vec * handlerMoveZ.GetValue() * maxTranslationSpeed * Time.deltaTime, Space.World);
-		}
+		Vector3 vecT = Vector3.zero;
+		vecT.x = (handlerMoveX != null) ? handlerMoveX.GetValue() : 0;
+		vecT.y = (handlerMoveY != null) ? handlerMoveY.GetValue() : 0;
+		vecT.z = (handlerMoveZ != null) ? handlerMoveZ.GetValue() : 0;
+		vecTranslate = Vector3.Lerp(vecTranslate, vecT, translationLerp);
 
-		if (handlerMoveY != null)
-		{
-			// calculate upwards (Y) direction of camera
-			vec = rotationBasisNode.up; vec.Normalize();
-			// translate upwards
-			transform.Translate(vec * handlerMoveY.GetValue() * maxTranslationSpeed * Time.deltaTime, Space.World);
-		}
-
-		if (handlerMoveX != null)
-		{
-			// calculate level sideways (X) direction of camera
-			vec = rotationBasisNode.right; vec.y = 0; vec.Normalize();
-			// translate forward
-			transform.Translate(vec * handlerMoveX.GetValue() * maxTranslationSpeed * Time.deltaTime, Space.World);
-		}
+		// calculate level forward (Z) direction of camera
+		vec = rotationBasisNode.forward; vec.y = 0; vec.Normalize();
+		// translate forward
+		transform.Translate(vec * vecTranslate.z * maxTranslationSpeed * Time.deltaTime, Space.World);
+		// calculate upwards (Y) direction of camera
+		vec = rotationBasisNode.up; vec.Normalize();
+		// translate upwards
+		transform.Translate(vec * vecTranslate.y * maxTranslationSpeed * Time.deltaTime, Space.World);
+		// calculate level sideways (X) direction of camera
+		vec = rotationBasisNode.right; vec.y = 0; vec.Normalize();
+		// translate forward
+		transform.Translate(vec * vecTranslate.x * maxTranslationSpeed * Time.deltaTime, Space.World);
 	}
 
 	private InputHandler handlerMoveX, handlerMoveY, handlerMoveZ;  // input handlers for moving
 	private InputHandler handlerRotateX, handlerRotateY;            // input handlers for rotating
-	private Vector3      vec;
+	private Vector3      vecTranslate, vecRotate, vec;
 }
