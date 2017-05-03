9791cd26353e5b6578c7165f6998e8a1a4bd6f56
Stefan Marks <stefan.marks.ac@gmail.com>
2017-05-03 21:35:34 +1200

    Removed editor code

--- Unity/Assets/SentienceLab/Scripts/MoCap/MoCapModel.cs
+++ Unity/Assets/SentienceLab/Scripts/MoCap/MoCapModel.cs
@@ -6,10 +6,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 namespace SentienceLab.MoCap
 {
 	[System.Serializable]
@@ -226,57 +222,4 @@ namespace SentienceLab.MoCap
 		private Actor                             actor;
 		private Dictionary<Bone, MoCapDataBuffer> dataBuffers;
 	}
-
-
-
-#if UNITY_EDITOR
-
-	/// <summary>
-	/// Class for rendering a bone translation entry in the editor
-	/// </summary>
-	/// 
-
-	[CustomPropertyDrawer(typeof(BoneNameTranslationEntry))]
-
-	public class BoneNameTranslationEntryDrawer : PropertyDrawer
-	{
-		// Draw the property inside the given rect
-		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-		{
-			EditorGUI.BeginProperty(position, label, property);
-			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-			// No indentation
-			int oldIndent = EditorGUI.indentLevel;
-			EditorGUI.indentLevel = 0;
-
-			// Calculate field positions
-			float labelWidth1 = 20;
-			float labelWidth2 = 5;
-			float space = (position.width - labelWidth1 - labelWidth2) / 5;
-			float x = position.x;
-			float w = space * 2;
-			Rect mocapRect = new Rect(x, position.y, w, position.height);
-			x += w; w = labelWidth1;
-			Rect arrowRect = new Rect(x, position.y, w, position.height);
-			x += w; w = space * 2;
-			Rect modelRect = new Rect(x, position.y, w, position.height);
-			x += w + labelWidth2; w = space;
-			Rect axisRect = new Rect(x, position.y, w, position.height);
-
-			// Draw fields - passs GUIContent.none to each so they are drawn without labels
-			EditorGUI.PropertyField(mocapRect, property.FindPropertyRelative("nameMocap"), GUIContent.none);
-			EditorGUI.LabelField(arrowRect, " →");
-			EditorGUI.PropertyField(modelRect, property.FindPropertyRelative("nameModel"), GUIContent.none);
-			EditorGUI.PropertyField(axisRect, property.FindPropertyRelative("axisTransformation"), GUIContent.none);
-
-			// restore indent level
-			EditorGUI.indentLevel = oldIndent;
-
-			EditorGUI.EndProperty();
-		}
-	}
-
-#endif
-
 }
