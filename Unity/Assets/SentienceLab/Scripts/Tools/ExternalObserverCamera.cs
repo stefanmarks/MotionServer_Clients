a500aae647696c073c9a5ad14e2973998178c0cb
Stefan Marks <stefanmarks@users.noreply.github.com>
2017-09-07 14:00:33 +1200

    Added external camera script and prefab

---
+++ Unity/Assets/SentienceLab/Scripts/Tools/ExternalObserverCamera.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class ExternalObserverCamera : MonoBehaviour
+{
+	public enum DisplayNumber
+	{
+		Display1, Display2, Display3, Display4, Display5, Displat6, Display7, Display8
+	}
+
+	[Tooltip("The camera that this observer camera should mirror")]
+	public Camera mainCamera = null;
+
+	[Tooltip("Interpolation factor for the external camera movement")]
+	public float lerpFactor = 0.1f;
+
+	[Tooltip("Check to avoid roll movement of the external camera")]
+	public bool noRoll = true;
+
+	[Tooltip("Which display should the external camera open up")]
+	public DisplayNumber displayNumber = DisplayNumber.Display2;
+
+
+	public void Start()
+	{
+		// use default camera if not explicitely stated
+		if (mainCamera == null)
+		{
+			mainCamera = Camera.main;
+		}
+
+		Display.onDisplaysUpdated += CheckForDisplay;
+		CheckForDisplay();
+	}
+	
+
+	public void Update()
+	{
+		UpdateObserverCamera(tempLerpFactorOverride > lerpFactor ? tempLerpFactorOverride : lerpFactor * Time.deltaTime);
+		tempLerpFactorOverride = 0;
+	}
+
+
+	private void UpdateObserverCamera(float _lerpFactor)
+	{
+		// adjust position and rotation of observer camera
+		Quaternion rotation = noRoll ? Quaternion.LookRotation(mainCamera.transform.forward, Vector3.up) : mainCamera.transform.rotation;
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _lerpFactor);
+		transform.position = Vector3.Lerp(transform.position, mainCamera.transform.position, _lerpFactor);
+	}
+
+	private void CheckForDisplay()
+	{
+		Camera camera = GetComponent<Camera>();
+
+		int displayIdx = (int)displayNumber;
+		if (Display.displays.Length > displayIdx)
+		{
+			Display d = Display.displays[displayIdx];
+			if (!d.active)
+			{
+				d.Activate();
+			}
+			// set some parameters
+			camera.targetDisplay = displayIdx;
+			camera.nearClipPlane = mainCamera.nearClipPlane;
+			camera.farClipPlane  = mainCamera.farClipPlane;
+
+			gameObject.SetActive(true);
+			tempLerpFactorOverride = 1; // immediately align
+		}
+		else
+		{
+			// no external display > shut down this node
+			gameObject.SetActive(false);
+		}
+	}
+
+	private float tempLerpFactorOverride = 0; /// Factor for temporarily overriding the global lerp factor
+}
